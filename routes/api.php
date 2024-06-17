09250c2acd8d240fdf2bb950da31900a1883395e
xTerrax <xTerrax17@gmail.com>
2024-06-17 15:27:48 -0600

    Rutas Crud users

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\AccessController;
 use App\Http\Controllers\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\RoleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,11 +22,28 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+Route::get('/users', [UserController::class, 'index']);
+Route::post('/users', [UserController::class, 'store']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+// routes/api.php
+
+Route::get('/roles', [RoleController::class, 'index']);
+Route::post('/roles', [RoleController::class, 'store']);
+Route::get('/roles/{id}', [RoleController::class, 'show']);
+Route::put('/roles/{id}', [RoleController::class, 'update']);
+Route::delete('/roles/{id}', [RoleController::class, 'destroy']);
+
+
+
 Route::prefix('v1')->group(function() {
     Route::prefix('users')->group(function() {
         // Route::post('register', [UsersController::class, 'createUser']);
         Route::post('log-in', [AccessController::class, 'logIn']);
-        
+
         Route::middleware(['active', 'auth:sanctum'])->group(function(){
             // Route::get('get-Myself', [UsersController::class, 'getMyself']);
             Route::delete('log-out', [AccessController::class, 'logOut']);
