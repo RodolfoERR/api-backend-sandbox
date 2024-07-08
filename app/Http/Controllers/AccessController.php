a7c04ddf7adba7b70c5a3eefdc1d1033fb4893ac
EccoEdd <coriaedd@gmail.com>
2024-07-07 20:07:53 -0600

    LogIn Normal User

--- app/Http/Controllers/AccessController.php
+++ app/Http/Controllers/AccessController.php
@@ -29,7 +29,7 @@ class AccessController extends Controller
             $token = $user->createToken('auth_token')->plainTextToken;
             $user->save();
 
-            return response()->json(['sucess' => true, 'message' => 'welcome', 'token' => $token], 200);
+            return response()->json(['success' => true, 'message' => 'welcome', 'token' => $token], 200);
         }
 
         if($user->role_id === 1 && $user->code !== null){
