1e13c3a70bcb2659150cf1c76765f0a042148e5f
Rodolfo Eimy Rodriguez Ramirez <rodolfoeimyrodriguez@gmail.com>
2024-07-10 18:22:35 -0600

    Updated design

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -97,7 +97,7 @@
 <body>
     <!-- Barra lateral -->
     <nav class="col-md-2 d-none d-md-block sidebar">
-        <div class="sidebar-sticky">
+        <div class="sidebar">
             <div class="profile-section">
                 <img src="https://www.movilzona.es/app/uploads-movilzona.es/2023/04/fto-perfil.jpg?x=480&y=375&quality=40" alt="Profile"> <!-- Ajusta el path de la imagen de perfil -->
                 <h4>Admin</h4>
@@ -105,22 +105,29 @@
             <ul class="nav flex-column">
                 <li class="nav-item">
                     <a class="nav-link active" href="userscrud">
-                        <i class="fas fa-user-plus"></i>
+                        <i class="fas fa-user"></i>
                         Usuarios
                         <hr>
                     </a>
                 </li>
                 <li>
                 <a class="nav-link active" href="refacciones">
-                        <i class="fas fa-user-plus"></i>
+               
                         Refacciones
                         <hr>
                     </a>
                 </li>
                 <li>
-                <a class="nav-link active" href="historyrefacciones">
-                        <i class="fas fa-user-plus"></i>
-                        Historial de Refacciones
+                <a class="nav-link active" href="types">
+                        
+                        Tipos
+                        <hr>
+                    </a>
+                </li>
+                <li>
+                <a class="nav-link active" href="locations">
+                        
+                        Ubicaciones
                         <hr>
                     </a>
                 </li>
