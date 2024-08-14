8cd8a3b0134ffa80ba6976b0a0b02f4c026481ed
Rodolfo Eimy Rodriguez Ramirez <rodolfoeimyrodriguez@gmail.com>
2024-08-14 13:20:56 -0600

    New view added to sidebar

--- resources/views/locations.blade.php
+++ resources/views/locations.blade.php
@@ -161,16 +161,10 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <div class="search-bar ml-auto">
-                <i class="fas fa-search"></i>
-                <input type="search" placeholder="Buscar">
+                <!--<i class="fas fa-search"></i>
+                <input type="search" placeholder="Buscar">-->
             </div>
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#"><i class="fas fa-bell"></i></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#"><i class="fas fa-comments"></i></a>
-                </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#" id="logoutButton">Cerrar Sesión</a>
                 </li>
@@ -211,6 +205,13 @@
                 <a class="nav-link active" href="locations">
                         
                         Ubicaciones
+                        <hr>
+                    </a>
+                </li>
+                <li>
+                    <a class="nav-link active" href="racks">
+                        
+                        Racks
                         <hr>
                     </a>
                 </li>
