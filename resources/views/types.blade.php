e083cb55dfbed3be211403fc83079f721af8b310
Rodolfo Eimy Rodriguez Ramirez <rodolfoeimyrodriguez@gmail.com>
2024-08-14 13:21:38 -0600

    New view added to sidebar

--- resources/views/types.blade.php
+++ resources/views/types.blade.php
@@ -186,6 +186,13 @@
                         <hr>
                     </a>
                 </li>
+                <li>
+                    <a class="nav-link active" href="racks">
+                        
+                        Racks
+                        <hr>
+                    </a>
+                </li>
             </ul>
         </div>
     </nav>
